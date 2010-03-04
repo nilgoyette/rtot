3b4a14b6c954dfaa89885d09bb681ce63a489b0b
patpickpaquet@hotmail.com <patpickpaquet@hotmail.com@700ad166-0f4e-11df-b42a-7dbe0d947131>
2010-03-04 17:15:26 +0000

    oops fixed it

---
+++ track/DebugGrabber.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "Grabber.h"
+#include "Timer.h"
+#include <iostream>
+
+class DebugGrabber : public Grabber
+{
+    Timer t;
+    bool started;
+    int framecount;
+public:
+
+    DebugGrabber(const int camId, const CvSize resolution, TripleBuffering& buffer)
+        : Grabber(camId, resolution, buffer) , started(false) , framecount(0)
+    {
+    }
+
+    ~DebugGrabber(void)
+    {
+    }
+
+    virtual void beforeGrab() throw() {
+        if(!started){
+            started = true;
+            t.start();
+        }
+        ++framecount;
+        if(framecount == 60){
+            double d = t.elapsed();
+            std::cout << 1/((d / 1000) /framecount ) << std::endl;
+            t.restart();
+        }
+    };
+    virtual void afterGrab() throw(){};
+};
