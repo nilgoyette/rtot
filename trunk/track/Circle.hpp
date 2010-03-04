1c2fadd4735767912f2a7adb7bdd6237cee016e8
nilgoyette <nilgoyette@700ad166-0f4e-11df-b42a-7dbe0d947131>
2010-03-04 20:37:46 +0000

    ;lkgndslg

--- trunk/track/Circle.hpp
+++ trunk/track/Circle.hpp
@@ -1,21 +1,31 @@
-#pragma once
+
+#ifndef __CIRCLE_H__
+#define __CIRCLE_H__
+
+#include <iostream>
 
 #include "cv.h"
 
-class Circle
-{
+class Circle {
+	public:
+		unsigned int radius_;
+		CvPoint center_;
+		CvRect bounds_;
+		bool empty_;
 
-public:
-    int radius;
-    CvPoint center;
-    CvRect bounds;
-    bool empty;
+		Circle(void) : empty_(true) { }
+		Circle(const CvRect bounds) {
+			init(bounds);
+		}
 
-    Circle(void)
-    {
-    }
-
-    ~Circle(void)
-    {
-    }
+		void init(const CvRect bounds) {
+			const unsigned int height = bounds.height;
+			const unsigned int width = bounds.width;
+			center_.x = bounds.x + (width / 2);
+			center_.y = bounds.y + (height / 2);
+			radius_ = std::max(height, width) / 2;
+			empty_ = false;
+		}
 };
+
+#endif
